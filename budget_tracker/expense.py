0d5f9ed6fcbf24e60069741176e383e7d65cb6eb
Aung Nyi Nyi Win <aungnyinyiwin37@gmail.com>
2026-09-24 11:23:33 +0630

    budget_tracker

--- budget_tracker/expense.py
+++ budget_tracker/expense.py
@@ -22,5 +22,41 @@ def total_expense():
             total += expense["amount"]
         return total
 
-    
+def total_expense_summary():
+    if not expenses:
+        print("no expense recorded.")    
+    else:
+        expense_total = {}
+        for expense in expenses:
+            category = expense["category"]
+            amount = expense["amount"]
+            if category not in expense_total:
+                expense_total[category]=0
+            expense_total[category]+= amount
+        return expense_total
+
+def expense_category_bysearch(expenses,category):
+    if not expenses:
+        return 0
+    else:
+        total = 0
+        for expense in expenses:
+            if expense["category"]== category:
+                total += expense["amount"]
+        return total
+
+def get_expense_number():
+    number = int(input("enter expense number:"))
+    if number <1 or number > len(expenses):
+        raise IndexError
+    return number -1
+
+def delete_expense():
+    if not expenses:
+        print("no expense recorded.")
+    else:
+
+
+
+
     
