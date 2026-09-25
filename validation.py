fb66cf0f931547af134d75b85d956c33d6c8eaaf
Aung Nyi Nyi Win <aungnyinyiwin37@gmail.com>
2026-09-25 11:40:25 +0630

    validation.py

---
+++ validation.py
@@ -0,0 +1,12 @@
+
+while True:
+    amount = int(input("enter amount:")) #သတိထားရမဲ့ နေရာပါ။amountသည် ၀ အောက်ငယ်ရင်invalid ‌amount လို့ပြပါမယ်။ပြီးရင် loopပြန်လည်ပြီး amount input ထပ်မေးပါမယ်။
+    if amount <=0:
+        print("invalid amount")
+
+
+
+
+    elif amount > 0:
+        print("valid amount") #amountသည် ၀ထက်ကြီးရင် break ဖြစ်ပါမယ်။
+        break
